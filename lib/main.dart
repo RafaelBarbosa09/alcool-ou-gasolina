beee3ccd6409835ebf2cb191ff3e9d7c2e9d9513
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2020-06-24 17:14:45 -0300

    app finalizado

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:alcool_ou_gasolina/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
